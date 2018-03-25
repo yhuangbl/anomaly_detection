a1c66aaaaa8293b947dcf3aeb3fd54bb3bbf8a3a
yhuangbl <yhuangbl@connect.ust.hk>
2018-03-24 22:59:10 -0700

    update utils; add SVM

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pickle
+from sklearn.metrics import roc_auc_score
 
 X = pickle.load(open("input/breast_cancerX", "rb"))
 Y = pickle.load(open("input/breast_cancerY", "rb"))
@@ -12,3 +13,6 @@ def analyze_outlier(df):
     first30_accuracy = (first30_0count / 30) * 100
     
     print ("Within the top 30 ranked cases (ranked according to the Outlier Factor), {} of the malignant cases (the outliers), comprising {}% of all malignant cases, were identified.".format(first30_0count, first30_accuracy))
+
+def calculate_roc_auc(y_true, y_pred):
+    print ("ROC AUC score: {}".format(roc_auc_score(y_true, y_pred)))
